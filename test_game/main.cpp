7e27fe83040698e762beb9a7ffcef61b9b7d15ae
ZxPura1 <flammemacaron@gmail.com>
2026-03-04 22:23:15 +0800

    textures and extension libraries_1

--- test_game/main.cpp
+++ test_game/main.cpp
@@ -1,32 +1,117 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 //#include <iostream>
 #include <string>
 
 /* Constants */
 //Screen dimesino constant
-constexpr int kScreenWidth{ 1000 };
-constexpr int kScreenHeight{ 1000 };
+constexpr int kScreenWidth{ 800 };
+constexpr int kScreenHeight{ 600 };
+
+/* Class Prototypes*/
+class LTexture
+{
+public:
+	//Initializes texture variables
+	/*
+	we set the values in the member initializer list as opposed to inside of the constructor itself. 
+	This is considered a good habit as it allows for better compiler optimization.
+	*/
+	LTexture():
+		mTexture{ nullptr },
+		mWidth{ 0 },
+		mHeight{ 0 }
+	{ }
+	
+
+	//Cleans up the texture variables
+	~LTexture()
+	{
+		destroy();
+	}
+
+	//Loads texture from disk
+	bool loadFromFile(std::string path)
+	{
+		//Clean up texture if it already exists
+		destroy();
+
+		//Load surface
+		if (SDL_Surface* loadedSurface = IMG_Load(path.c_str()); loadedSurface == nullptr)
+		{
+			SDL_Log("Unable to load image %s! SDL_image error: %s\n", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			//Create texture from surface
+			if (mTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface); mTexture == nullptr)
+			{
+				SDL_Log("Unable to create texture from loaded pixels! SDL error: %s\n", SDL_GetError());
+			}
+			else
+			{
+				//get image dimensions
+				mWidth = loadedSurface->w;
+				mHeight = loadedSurface->h;
+			}
+			
+			//Clean up loaded surface
+			SDL_DestroySurface(loadedSurface);
+		}
+
+		//return success if texture loaded
+		return mTexture != nullptr;
+
+	}
+
+	//Cleans up texture
+	void destroy();
+
+	//Draws texture
+	void render(float x, float y);
+
+	//gets texture attributes
+	int getWidth();
+	int getHeight();
+	bool isLoaded();
+
+private:
+	//contains texture data
+	SDL_Texture* mTexture;
+
+	//Texture dimensions
+	int mWidth;
+	int mHeight;
+
+};
 
 /* Function Prototypes */
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
+////Starts up SDL and creates window
+//bool init();
+//
+////Loads media
+//bool loadMedia();
+//
+////Frees media and shuts down SDL
+//void close();
 
 /* Global Variables */
 //The window we'll be rendering to
 SDL_Window* gWind{ nullptr };
 
-//The surface contained by the window
-SDL_Surface* gScreenSurface(nullptr);
+//The renderer used to draw to the window
+SDL_Renderer* gRenderer{ nullptr };
 
-//The image we will show onto the screen
-SDL_Surface* gHelloWorld{ nullptr };
+//The PNG image we will render
+LTexture gPngTexture;
+
+
+////The surface contained by the window
+//SDL_Surface* gScreenSurface(nullptr);
+//
+////The image we will show onto the screen
+//SDL_Surface* gHelloWorld{ nullptr };
 
 /* Function Implementations */
 bool init()
